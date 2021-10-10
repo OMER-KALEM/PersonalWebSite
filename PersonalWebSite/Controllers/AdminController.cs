8c7fa2337a561f4e44b2fadd8cda66dd197a7956
Ömer KALEM <omer.kalem@ogr.sakarya.edu.tr>
2021-10-10 15:16:36 +0300

    Admin Layout - Read from Context

---
+++ PersonalWebSite/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using PersonalWebSite.Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalWebSite.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var deger = context.MainScreens.ToList();
+            return View(deger);
+        }
+    }
+}
